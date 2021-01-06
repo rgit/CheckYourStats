2723a9a32245ce29c0a6748fc093b69666c2c32b
Demenkop <46030746+Demenkop@users.noreply.github.com>
2021-01-06 21:54:08 +0000

    i need this to be in master

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,8 +2,10 @@ from aiogram import executor
 from bot.misc import dp
 from bot.utils import Model
 import bot.handlers
+from sys import argv
 
 model = Model()
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=("--skip" in argv))
+

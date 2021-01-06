5cf66cfaa0393b90dc6d2893fec583c19a1eb688
Kirill Feschenko <swipduces@yandex.com>
2021-01-06 04:20:34 +0300

    Updated a config example.

---
+++ bot/config.py.example.py
@@ -0,0 +1,10 @@
+class Config:
+    API_TOKEN = ""
+    REMOVE_COMMAND_AFTER_USAGE = True
+    ANTISPAM = True
+    DATASET_PATH = ""
+    POSTGRES_USER = ""
+    POSTGRES_PASSWORD = ""
+    POSTGRES_HOST = ""
+    POSTGRES_PORT = ""
+    POSTGRES_DB = ""
